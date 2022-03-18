6a4b90e1759f5903ef14d251aaab73cb3e0adb9d
Rizqy Fachri <rizqyfachri@gmail.com>
2022-03-18 17:53:40 +0700

    finish desktop

--- js/main.js
+++ js/main.js
@@ -1,6 +1,33 @@
+// Hamburger for Phone
 const hamburger = document.getElementById('hamburger');
 const navUL = document.getElementById('nav-ul');
 
 hamburger.addEventListener('click', () => {
     navUL.classList.toggle('show');
 });
+
+// When the user clicks on the button, toggle between hiding and showing the dropdown content
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function myFunction2() {
+  document.getElementById("myDropdown2").classList.toggle("show");
+}
+
+function myFunction3() {
+  document.getElementById("myDropdown3").classList.toggle("show");
+}
+// Close dropdown when ser click outside
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
